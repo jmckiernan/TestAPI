5a442dfcf0532c5e7490a05261a90b3045d42ff9
jmckiernan <59330422+jmckiernan@users.noreply.github.com>
2021-03-23 00:07:04 -0700

    update

--- server.js
+++ server.js
@@ -6,13 +6,12 @@ const mongoose = require('mongoose')
 // const serverRouter = express.Router()
 
 
-mongoose.connect('mongodb://localhost/subscribers', { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost/subscribers', { useNewUrlParser: true }, { useUnifiedTopology: true })
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
 
 app.use(express.json())
-
 
 const subscribersRouter = require('./routes/subscribers')
 app.use('/subscribers', subscribersRouter)
